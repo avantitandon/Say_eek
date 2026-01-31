df3ed2986f867e0ecbfaa57009181de9a955d45e
Martin Calcaterra <martin.calcaterra@mail.utoronto.ca>
2026-01-30 22:50:09 -0500

    First full scoring draft
    more points near center of photo, raycasts ignore collisions

--- Assets/Scripts/AimStateManager.cs
+++ Assets/Scripts/AimStateManager.cs
@@ -46,6 +46,9 @@ public class ThirdPersonCamera : MonoBehaviour
 
 
     int ghostHit = 0;
+    int ghostScore = 0;
+
+    public LayerMask ignoreLayers;
 
 
     void Start()
@@ -64,7 +67,9 @@ public class ThirdPersonCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        UItext.text = "Last Photo Score: " + photoScore.ToString() + "\nGhost Hit: " + ghostHit.ToString();
+        //UItext.text = "Last Photo Score: " + photoScore.ToString() + "\nGhost Hit: " + ghostScore.ToString();
+        UItext.text = "Last Photo Score: " + ghostScore.ToString();
+
         if (photoAction.WasPressedThisFrame())
         {
             TakePhoto();
@@ -140,27 +145,73 @@ public class ThirdPersonCamera : MonoBehaviour
         cameraAudio.PlayOneShot(shutter);
 
 
+        ghostScore = 0;
+
+        int step = Screen.width / 30;
+        Debug.Log(step);
+        Debug.Log(Screen.width);
+        Debug.Log(Screen.height);
+
         // needs to be recalculated for new camera position
         Vector3 pos = new Vector3(0, 0, 0);
-        Ray ray = photoCamera.ScreenPointToRay(pos);
-
+        Vector3 center = new Vector3(Screen.width, Screen.height, 0);
+        float radius = Screen.width / 4;
+        int pixel = 0;
+        Ray ray;
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
+        while (pos.x < (Screen.width * 2) && pos.y < (Screen.height * 2))
         {
-            if (hit.collider.gameObject.layer == 6)
+
+            ray = photoCamera.ScreenPointToRay(pos);
+            if (Physics.Raycast(ray, out hit, 1000f, layerMask: ~ignoreLayers))
             {
-                ghostHit = 1;
+
+                if (hit.collider.gameObject.layer == 6)
+                {
+                    ghostScore = ghostScore + 1;
+
+                    if (hit.collider.gameObject.GetComponent<ballscript>().spinning)
+                    {
+                        ghostScore = ghostScore + 2;
+                    }
+
+                }
+            }
+
+            if ((pos - center).magnitude < radius)
+            {
+                Debug.Log("centerhit");
+                pixel = pixel + (step / 3);
+                pos.x = pixel % (Screen.width * 2);
+                pos.y = step * (pixel / (Screen.width * 2));
             }
             else
             {
-                ghostHit = 2;
+                pixel = pixel + step;
+                pos.x = pixel % (Screen.width * 2);
+                pos.y = step * (pixel / (Screen.width * 2));
             }
         }
-        else
-        {
-            ghostHit = 3;
-        }
+       
+
+        //RaycastHit hit;
+
+        //if (Physics.Raycast(ray, out hit))
+        //{
+        //    if (hit.collider.gameObject.layer == 6)
+        //    {
+        //        ghostHit = 1;
+        //    }
+        //    else
+        //    {
+        //        ghostHit = 2;
+        //    }
+        //}
+        //else
+        //{
+        //    ghostHit = 3;
+        //}
 
     }
 
