fd794df2a28821ff82ac85611b7f9babe3e81f80
DominikKos299 <dominikkosciolek05@gmail.com>
2026-02-24 02:43:50 -0500

    Added camera capture event in CCmonolith

--- Assets/Scripts/CCMonolith.cs
+++ Assets/Scripts/CCMonolith.cs
@@ -79,6 +79,9 @@ public class CameraControllerMonolith : MonoBehaviour
     [SerializeField] private GameObject hearts3;
     [SerializeField] private GameObject backplate;
 
+    // Wwise event for camera capture
+    [SerializeField] private AK.Wwise.Event playCameraCapture;
+
 
 
     void Start()
@@ -198,6 +201,7 @@ public class CameraControllerMonolith : MonoBehaviour
         }
         photoCamera.Render();
         Debug.Log("Screenshot captured");
+        playCameraCapture.Post(gameObject);
         apertureFx?.PlayShutter();
         cameraAudio.PlayOneShot(shutter);
 
