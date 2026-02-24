42faff5565e295a90452187855574fa774cde7d4
DominikKos299 <dominikkosciolek05@gmail.com>
2026-02-23 20:06:14 -0500

    Wwise view close view open and zoom soundbanks and events and ZoomPercentageParameter

--- Say_eek_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ Say_eek_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,7 +14,18 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_CAMERACAPTURE = 3730852459U;
+        static const AkUniqueID PLAY_CAMERAVIEWCLOSE = 3080007124U;
+        static const AkUniqueID PLAY_CAMERAVIEWOPEN = 2617086320U;
+        static const AkUniqueID PLAY_CAMERAZOOMIN = 187507839U;
+        static const AkUniqueID PLAY_CAMERAZOOMOUT = 1363090U;
+        static const AkUniqueID STOP_CAMERAZOOMIN = 3484989405U;
+        static const AkUniqueID STOP_CAMERAZOOMOUT = 3252179076U;
     } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CAMERAZOOMPERCENTAGE = 965791921U;
+    } // namespace GAME_PARAMETERS
 
     namespace BUSSES
     {
