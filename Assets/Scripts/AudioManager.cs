55887b9f2847f75b495b3b63383c5d113170a5d4
DominikKos299 <dominikkosciolek05@gmail.com>
2026-02-26 13:16:28 -0500

    Audio prefab tweak

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AK.Wwise.Event playMusic;
+    [SerializeField] private AK.Wwise.Event playAmbience;
+}
