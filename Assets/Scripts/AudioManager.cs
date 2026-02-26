1836161bde4178ac5dcfe42a7ac70f8a671f5898
DominikKos299 <dominikkosciolek05@gmail.com>
2026-02-26 16:18:41 -0500

    Music and Ambience

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,6 +2,48 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
     [SerializeField] private AK.Wwise.Event playMusic;
     [SerializeField] private AK.Wwise.Event playAmbience;
+    [SerializeField] private AK.Wwise.Event playFootsteps;
+    [SerializeField] private AK.Wwise.Event stopFootsteps;
+    [SerializeField] private AK.Wwise.Event playCameraCapture;
+    // Flag to track if footsteps are currently playing
+    private bool isPlayingFootsteps = false;
+
+
+
+
+    void Start()
+    {
+        playMusic.Post(player);
+        playAmbience.Post(player);
+    }
+    public void HandleFootsteps(Vector3 movementDirection)
+    {
+        bool isMoving = movementDirection.magnitude > 0.1f; 
+
+        if (isMoving)
+        {
+            Debug.Log("boing");
+            if (!isPlayingFootsteps)
+            {
+                playFootsteps.Post(player);
+                isPlayingFootsteps = true;
+            }
+        }
+        else
+        {
+            if (isPlayingFootsteps)
+            {
+                stopFootsteps.Post(player);
+                isPlayingFootsteps = false;
+            }
+        }
+    }
+    public void playCapture()
+    {
+        playCameraCapture.Post(player);
+    }
 }
+    
