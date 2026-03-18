3d2b9e70e66dd6b093646956b4cde4982b14c5cb
DominikKos299 <dominikkosciolek05@gmail.com>
2026-03-18 01:21:50 -0400

    Connected audio manager to zoom and photo manager. camera capture works, music works

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMB_WATER_001 = 4200714142U;
+        static const AkUniqueID PLAY_AMBIENCE1 = 1117071627U;
+        static const AkUniqueID PLAY_CAMERACAPTURE = 3730852459U;
+        static const AkUniqueID PLAY_CAMERAVIEWCLOSE = 3080007124U;
+        static const AkUniqueID PLAY_CAMERAVIEWOPEN = 2617086320U;
+        static const AkUniqueID PLAY_CAMERAZOOMIN = 187507839U;
+        static const AkUniqueID PLAY_CAMERAZOOMOUT = 1363090U;
+        static const AkUniqueID PLAY_ENDSCOREMUSIC = 1600600422U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_MAINMUSIC = 1346240278U;
+        static const AkUniqueID STOP_AMBIENCE1 = 3294285673U;
+        static const AkUniqueID STOP_CAMERAZOOMIN = 3484989405U;
+        static const AkUniqueID STOP_CAMERAZOOMOUT = 3252179076U;
+        static const AkUniqueID STOP_ENDSCOREMUSIC = 261268628U;
+        static const AkUniqueID STOP_FIRST_DANCE_IN_HELL___MAR10 = 4038005146U;
+        static const AkUniqueID STOP_FOOTSTEPS = 2963349357U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AMBIENCESPREAD = 2341644420U;
+        static const AkUniqueID CAMERAVIEW = 1232093211U;
+        static const AkUniqueID CAMERAZOOMPERCENTAGE = 965791921U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
