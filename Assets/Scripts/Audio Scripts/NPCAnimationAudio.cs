e901e5c1fc6ea3e6743bbbc5062febffea8bf29a
DominikKos299 <dominikkosciolek05@gmail.com>
2026-03-31 06:20:23 -0400

    Beezaka idkle fixed,

---
+++ Assets/Scripts/Audio Scripts/NPCAnimationAudio.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class NPCAnimationAudio : MonoBehaviour
+{
+    public AudioManager audioManager;
+    public Animator animator;
+    public void NPCFootstep()
+    {
+        if (animator == null) //this is regular footstep, if there is no reason for different animation states. Ref in audiomanager as "Defaul"
+        {
+            audioManager.HandleFootstep(this.gameObject, "Default");
+            return;
+        }
+        
+        string currentAnimation = "";
+
+        //this is for when an animation state is needed
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("beezaka idle")) // this is beezaka idle
+        {
+            currentAnimation = "Idle";
+        }
+        else
+        {
+            currentAnimation = "Default";
+        }
+        
+        //this is calling the method IF there is an animator needed for the animation state 
+        audioManager.HandleFootstep(this.gameObject, currentAnimation);
+    }
+}
