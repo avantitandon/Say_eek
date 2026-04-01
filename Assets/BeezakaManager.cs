93c33f38b148f15a9f7341660cada197d75b98f6
Bianca Lee <bronkosaurb@gmail.com>
2026-03-31 22:31:08 -0400

    beezaka just barely working ohhhhhhhhhhhh

--- Assets/BeezakaManager.cs
+++ Assets/BeezakaManager.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
-
+using UnityEngine.AI;
 public class BeezakaManager : MonoBehaviour
 {   
     [SerializeField] private Beehaviours beehaviours;
-    public UnityEngine.AI.NavMeshAgent bzNavMesh { get; private set; }
+    
+    //public NavMeshAgent bzNavMesh;
     public enum WhatShouldBeezDo
     {
         InHell,
@@ -17,46 +18,65 @@ public class BeezakaManager : MonoBehaviour
     float spawnTime = 0f;
     public GameObject beezakaportal;
     float velocity;
+    public GameObject beezaka;
+    
+    private int whatDIDbeezdo;
     void Start()
     {
         EventManager.BEEZAKAPOCALYPSENOW += BZisNigh;
         EventManager.GazeboKissing += AttendCeremony;
         bool BeezSpawned = false;
         whatShouldBeezDo = WhatShouldBeezDo.InHell;
-        bzNavMesh = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        //bzNavMesh = GetComponent<NavMeshAgent>();
+        whatDIDbeezdo = (int)whatShouldBeezDo;
+        
         
     }
 
     // Update is called once per frame
     private void BZisNigh()
     {
-        whatShouldBeezDo = WhatShouldBeezDo.InHell;
-        Debug.Log("beezaka start spawning");
-        Vector3 deltaPos = beezakaportal.transform.position - transform.position;
+        whatShouldBeezDo = WhatShouldBeezDo.Spawning;
+
         Debug.Log("bz manager bzisnigh!!!");
+        
         switch(whatShouldBeezDo)
-        {
-            case (WhatShouldBeezDo.Spawning):
+        {   
+            case WhatShouldBeezDo.InHell:
                 break;
-            case (WhatShouldBeezDo.InHell):
-                while (spawnTime < 1.0f)
+            case WhatShouldBeezDo.Spawning:
+                    Debug.Log("beezaka start spawning");
+                //while (whatShouldBeezDo == WhatShouldBeezDo.Spawning)
+                    beezaka.transform.position = beezakaportal.transform.position;
+                    whatShouldBeezDo  =  WhatShouldBeezDo.Creeping;
+                break;/*
                     
-                        Debug.Log("beezaka going up the hellevator");
+                    
+                    
+                    Vector3 deltaPos = beezakaportal.transform.position - beezaka.transform.position;
+                    //velocity = bzNavMesh.velocity.magnitude;
+                    Debug.Log("beezaka going up the hellevator");
+                        
+                    if (spawnTime < 1.0f)
+                    {    Debug.Log("beezaka still going up the elevator");
                         spawnTime += Time.deltaTime;
-                        transform.position += deltaPos * Time.deltaTime;
-                            
-                        if (velocity > 0f)
-                        {    Debug.Log("beezaka still going up the elevator");
-                            return;
-                        }
-                        else
-                        {
-                            Debug.Log("beezaka has surely made it to the top floor by now");
-                            whatShouldBeezDo = WhatShouldBeezDo.Creeping;
-                            break;
-                        }
-                
+                        beezaka.transform.position += deltaPos * Time.deltaTime;
+                        return;
+                    }
+                    else
+                    {
+                        Debug.Log("beezaka has surely made it to the top floor by now");
+                        whatShouldBeezDo = WhatShouldBeezDo.Creeping;
+                        break;
+                    }
+                break;*/
+            
         }
+    }
+    private void B2Bcommunication()
+    {   
+        
+        beehaviours.WhatIsBeezDoing(whatShouldBeezDo);
     }
     private void AttendCeremony()
     {   
@@ -69,19 +89,23 @@ public class BeezakaManager : MonoBehaviour
     }
     void Update()
     {
+        if ((int)whatShouldBeezDo != whatDIDbeezdo)
+        {
+            B2Bcommunication();
+            whatDIDbeezdo = (int)whatShouldBeezDo;
+        }
         
-        beehaviours.WhatIsBeezDoing(whatShouldBeezDo);
-        velocity = bzNavMesh.velocity.magnitude;
+        //velocity = bzNavMesh.velocity.magnitude;
 
         switch(whatShouldBeezDo)
         {
-            case (WhatShouldBeezDo.Spawning):
+            case WhatShouldBeezDo.Spawning:
                 Debug.Log("void update spawning confirm");
                 break;
-            case (WhatShouldBeezDo.Creeping):
+            case WhatShouldBeezDo.Creeping:
                 Debug.Log("void update creeping confirm");
                 break;
-            case (WhatShouldBeezDo.Stalking):
+            case WhatShouldBeezDo.Stalking:
                 Debug.Log("void update stalking confirm");
                 break;
             
