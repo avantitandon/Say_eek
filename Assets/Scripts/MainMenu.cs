48b6016a58a551dd94411e141db44ab8fd2cc357
ncheeu <nicolecheeu@gmail.com>
2026-04-06 18:00:56 -0400

    credits
    added credits

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,8 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject CreditsPopup;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("mainmap");
+    }
+
+    public void OpenCredits()
+    {
+        CreditsPopup.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        CreditsPopup.SetActive(false);
     }
 }
